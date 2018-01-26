a68cb4cdb3c49c991bec03eda8d111247c8f1d9e
James Irwin <irwineffect@gmail.com>
2018-01-26 13:09:41 -0800

    added basic pin manipulation

---
+++ teensy/pin_control.c
@@ -0,0 +1,115 @@
+#include "pin_control.h"
+#include <stdint.h>
+
+const PORT port_a = {(void*) 0x40049000, (void*) 0x400FF000},
+           port_b = {(void*) 0x4004A000, (void*) 0x400FF040},
+           port_c = {(void*) 0x4004B000, (void*) 0x400FF080},
+           port_d = {(void*) 0x4004C000, (void*) 0x400FF0C0},
+           port_e = {(void*) 0x4004D000, (void*) 0x400FF100};
+
+
+void pin_set_mux(PORT *port, uint8_t pin, uint8_t mux)
+{
+    // valid values for pin is 0-31d
+    // valid values for mux is 0-111b
+    if (pin >= 32 || mux > 0b111)
+    {
+        // probably should hcf?
+        return;
+    }
+
+    port->pin_control_regs->pins[pin] |= (mux << 8);
+}
+
+void pin_set_drive_strength(PORT *port, uint8_t pin, uint8_t
+        drive)
+{
+    // valid values for pin is 0-31d
+    if (pin >= 32)
+    {
+        // probably should hcf?
+        return;
+    }
+
+    if (drive == 0)
+    {
+        port->pin_control_regs->pins[pin] &= ~(1 << 6);
+    }
+    else if (drive == 1)
+    {
+        port->pin_control_regs->pins[pin] |= (1 << 6);
+    }
+    else
+    {
+        // probably should hcf?
+        return;
+    }
+}
+
+void pin_set_data_direction(PORT *port, uint8_t pin, uint8_t out_nin)
+{
+    // valid values for pin is 0-31d
+    if (pin >= 32)
+    {
+        // probably should hcf?
+        return;
+    }
+
+    //valid values for out_nin is 0,1
+    if (out_nin == 0)
+    {
+        port->gpio_control_regs->PDDR &= ~(1 << pin);
+    }
+    else if (out_nin == 1)
+    {
+        port->gpio_control_regs->PDDR |= (1 << pin);
+    }
+    else
+    {
+        // probably should hcf?
+        return;
+    }
+}
+
+uint8_t pin_read(PORT *port, uint8_t pin)
+{
+    // valid values for pin is 0-31d
+    if (pin >= 32)
+    {
+        // probably should hcf?
+        return 0;
+    }
+
+    if (port->gpio_control_regs->PDIR & (1 << pin))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void pin_write(PORT *port, uint8_t pin, uint8_t state)
+{
+    // valid values for pin is 0-31d
+    if (pin >= 32)
+    {
+        // probably should hcf?
+        return;
+    }
+
+    if (state == 0)
+    {
+        port->gpio_control_regs->PCOR = (1 << pin);
+    }
+    else if (state == 1)
+    {
+        port->gpio_control_regs->PSOR = (1 << pin);
+    }
+    else
+    {
+        // probably should hcf?
+        return;
+    }
+}
---
+++ teensy/pin_control.h
@@ -0,0 +1,39 @@
+#ifndef PIN_CONTROL_H
+#define PIN_CONTROL_H
+
+#include <stdint.h>
+
+typedef struct
+{
+    uint32_t pins[32];
+} PIN_CONTROL_REGISTER_MAP;
+
+typedef struct
+{
+    uint32_t PDOR; // 0x00 data output
+    uint32_t PSOR; // 0x04 set output
+    uint32_t PCOR; // 0x08 clear output
+    uint32_t PTOR; // 0x0C toggle output
+    uint32_t PDIR; // 0x10 data input
+    uint32_t PDDR; // 0x14 data direction register
+} GPIO_REGISTER_MAP;
+
+typedef struct
+{
+    PIN_CONTROL_REGISTER_MAP *pin_control_regs;
+    GPIO_REGISTER_MAP *gpio_control_regs;
+} PORT;
+
+extern const PORT port_a,
+                  port_b,
+                  port_c,
+                  port_d,
+                  port_e;
+
+void pin_set_mux(PORT *port, uint8_t pin, uint8_t mux);
+void pin_set_drive_strength(PORT *port, uint8_t pin, uint8_t drive);
+void pin_set_data_direction(PORT *port, uint8_t pin, uint8_t out_nin);
+uint8_t pin_read(PORT *port, uint8_t pin);
+void pin_write(PORT *port, uint8_t pin, uint8_t state);
+
+#endif

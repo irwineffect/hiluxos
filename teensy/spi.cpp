7736574de7eb563778b6805101f982ba3197edb0
James Irwin <irwineffect@gmail.com>
2018-03-24 14:40:51 -0700

    tweaked spi initialization

--- teensy/spi.cpp
+++ teensy/spi.cpp
@@ -7,12 +7,18 @@ spi spi_0(0x4002C000);
 void spi::init()
 {
     // set master mode
-    regs->MCR = (1 << 31);
+    regs->MCR |= (1 << 31);
 
     // disable tx/rx fifos
     regs->MCR |= (1 << 13) | (1 << 12);
 
     // TODO: Setup pins
+
+
+    // TODO: Set clock divider
+
+    // clear the halt bit
+    regs->MCR  &= ~(1 << 0);
 }
 
 void spi::send_byte(const uint8_t b)
